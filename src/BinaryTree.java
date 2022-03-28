84cb8d92088445f21355c0dea96647ecf265f67c
Iker Goni <igoni@cpp.edu>
2022-03-28 09:50:35 -0700

    completed getNumberOfNodes() and getNumberOfNodes(BinaryNode<T> node) in BinaryTree.java

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -163,7 +163,12 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The number of nodes in the "whole" tree */
    public int getNumberOfNodes()
    {
-      return 0;
+      if(root == null){
+         return 0;
+      }
+      else{
+         return getNumberOfNodes(getRootNode());
+      }
    } // end getNumberOfNodes
    
    /** A Recursive Method in the BinaryTree Class   
@@ -171,7 +176,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The number of nodes in the subtree rooted at this node. */
    private int getNumberOfNodes(BinaryNode<T> node)
    {
-      return 0;
+      return 1 + getNumberOfNodes(node.getLeftChild()) + getNumberOfNodes(node.getRightChild());
    } // end getNumberOfNodes
    
    /** The following calls getNumberOfNodes_binaryNodeMethod() which is a recursive binaryNode class method
