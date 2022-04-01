ae6671e861ad7fd007380f89ccc00f0d23c9a2b0
Alex Mariano <amarianocello@gmail.com>
2022-04-01 16:43:04 -0700

    Fixed BinaryTree.getHeight_callBinaryNodeMethod - Alex

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -168,7 +168,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The height of the "whole" tree. */
    public int getHeight_callBinaryNodeMethod()
    {
-      if(root == null)
+      if(root != null)
          return root.getHeight_binaryNodeMethod(root);
       return 0;
    } // end getHeight_callBinaryNodeMethod
