c3ae0749b2ff923e36ae6e026e39eda6fe18c2a6
Iker Goni <igoni@cpp.edu>
2022-04-05 21:38:59 -0700

    set up JUnitTesting.java and the methods that need to be tested

---
+++ JUnitTesting..java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class JUnitTesting{
+
+    public void tryPostOrderTraverse(){
+
+    }
+    public void tryPostOrderTraverse_callBinaryNodeMethod(){
+
+    }
+
+    public void tryGetHeight_callBinaryMethod(){
+
+    }
+    public void tryGetNumberOfNodes(){
+
+    }
+    public void tryCreateTree2(){
+        
+    }
+    
+}
