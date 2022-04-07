61a53071fab88439428a55082a51c92abd2cff25
Alex Mariano <amarianocello@gmail.com>
2022-04-06 19:01:57 -0700

    @Test headers and fail message for tryECBinaryTree() on JUnitTesting.java

--- src/JUnitTesting.java
+++ src/JUnitTesting.java
@@ -1,4 +1,3 @@
-//package src;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -9,30 +8,31 @@ import static org.junit.Assert.fail;
 
 public class JUnitTesting{
 
-        BinaryTree<String> fTree = new BinaryTree<>("F");
-		BinaryTree<String> gTree = new BinaryTree<>("G");
-		BinaryTree<String> hTree = new BinaryTree<>("H");
-		BinaryTree<String> bTree = new BinaryTree<>("B");
+    BinaryTree<String> fTree = new BinaryTree<>("F");
+    BinaryTree<String> gTree = new BinaryTree<>("G");
+    BinaryTree<String> hTree = new BinaryTree<>("H");
+    BinaryTree<String> bTree = new BinaryTree<>("B");
 
-		// Subtrees:
-		BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
-		BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
-		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+    // Subtrees:
+    BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+    BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
+    BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
 
-        BinaryTree<String> tree = new BinaryTree<>("A", bTree, cTree);
+    BinaryTree<String> tree = new BinaryTree<>("A", bTree, cTree);
 
-        BinaryNode<String> fNode = new BinaryNode<>("F");
-		BinaryNode<String> gNode = new BinaryNode<>("G");
-        BinaryNode<String> hNode = new BinaryNode<>("H");
-		BinaryNode<String> bNode = new BinaryNode<>("B");
+    BinaryNode<String> fNode = new BinaryNode<>("F");
+    BinaryNode<String> gNode = new BinaryNode<>("G");
+    BinaryNode<String> hNode = new BinaryNode<>("H");
+    BinaryNode<String> bNode = new BinaryNode<>("B");
 
-		// Subtrees:
-        BinaryNode<String> dNode = new BinaryNode<>("D", fNode, null);
-		BinaryNode<String> eNode = new BinaryNode<>("E", gNode, hNode);
-        BinaryNode<String> cNode = new BinaryNode<>("C", dNode, eNode);
+    // Subtrees:
+    BinaryNode<String> dNode = new BinaryNode<>("D", fNode, null);
+    BinaryNode<String> eNode = new BinaryNode<>("E", gNode, hNode);
+    BinaryNode<String> cNode = new BinaryNode<>("C", dNode, eNode);
 
-        BinaryNode<String> nodeTree = new BinaryNode<>("A", bNode, cNode);
+    BinaryNode<String> nodeTree = new BinaryNode<>("A", bNode, cNode);
 
+    @Test
     public void tryPostOrderTraverse(){
         assertEquals("BFDGHECA", tree.postorderTraverse_test());
         assertEquals("BFDGHECA", tree.postorderTraverse_test(tree.getRootNode()));
@@ -40,20 +40,23 @@ public class JUnitTesting{
         assertEquals("BFDGHECA", nodeTree.postorderTraverse_binaryNodeMethod_test());
 
     }
+    @Test
     public void tryGetHeightMethods(){
         assertEquals(4, tree.getHeight_callBinaryNodeMethod());
-        assertEquals(4, nodeTree.getHeight_binaryNodeMethod(tree.getRootNode()));
+        assertEquals(4, nodeTree.getHeight_BinaryNodeMethod());
     }
+    @Test
     public void tryGetNumberOfNodes(){
         assertEquals(8, tree.getNumberOfNodes());
         assertEquals(8, tree.getNumberOfNodes_test(tree.getRootNode()));
     }
+    @Test
     public void tryECBinaryTree(){
         try{
             BinaryTree<Character> extraCreditTree = new BinaryTree<Character>("ABDHIECFGJK", "HDIBEAFCJGK");
         }
         catch (Exception e){
-            throw e;
+            fail("Failure: tryECBinaryTree() threw an exception: " + e);
         }
     }
 
